53f5e08529ee143444fe394d0964ac89d5c5f5a8
Maria <ec39sp@yandex.ru>
2023-09-15 22:34:53 +0200

    hw2

---
+++ hw2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[,] array = new int[,]
+        {
+            { 1, 4, 7, 2 },
+            { 1, 2, 1, 1 },
+            { 8, 4, 2, 4 },
+            { 5, 2, 6, 7 }
+        };
+
+        Console.WriteLine("Исходный массив:");
+
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                Console.Write(array[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+
+        int minRowIndex = 0;
+        int minRowSum = int.MaxValue;
+
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            int rowSum = 0;
+
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                rowSum += array[i, j];
+            }
+
+            if (rowSum < minRowSum)
+            {
+                minRowSum = rowSum;
+                minRowIndex = i;
+            }
+        }
+
+        Console.WriteLine("Номер строки с наименьшей суммой элементов: " + (minRowIndex + 1));
+
+        Console.ReadLine();
+    }
+}
